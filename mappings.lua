b42932a3b86ea0317c1a35ff1b0ffced647fae94
aik2 <aik2mlj@gmail.com>
2023-11-14 01:27:07 +0400

    wip

--- mappings.lua
+++ mappings.lua
@@ -8,15 +8,15 @@ return {
   n = {
     -- second key is the lefthand side of the map
 
-    -- navigate buffer tabs with `H` and `L`
-    -- L = {
-    --   function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
-    --   desc = "Next buffer",
-    -- },
-    -- H = {
-    --   function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
-    --   desc = "Previous buffer",
-    -- },
+    -- navigate buffer tabs with `alt-h` and `alt-l`
+    ["<M-l>"] = {
+      function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+      desc = "Next buffer",
+    },
+    ["<M-h>"] = {
+      function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+      desc = "Previous buffer",
+    },
 
     -- mappings seen under group name "Buffer"
     ["<leader>bD"] = {
@@ -32,9 +32,30 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+
+    -- ["<"] = {"<<"},
+    -- [">"] = {">>"},
+    U = { "<C-r>" },
+    H = { "g^" },
+    L = { "g$" },
+    J = { "5gj" },
+    K = { "5gk" },
+    ["<M-j>"] = { ":m .+1<CR>==" },
+    ["<M-k>"] = { ":m .-2<CR>==" },
+    ["<M-J>"] = { ":t .<CR>" },
+    ["<M-K>"] = { ":t .-1<CR>" },
+    ["<leader>k"] = { function() vim.lsp.buf.hover() end, desc = "Hover symbol details" },
   },
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
+  },
+  x = {
+    J = { "5gj" },
+    K = { "5gk" },
+    ["<M-j>"] = { ":m'>+<CR>==gv" },
+    ["<M-k>"] = { ":m-2<CR>==gv" },
+    ["<M-J>"] = { ":t'><CR>gv" },
+    ["<M-K>"] = { ":t-1<CR>gv" },
   },
 }
--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,11 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin", enabled = true },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim", enabled = true },
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.lsp.lsp-signature-nvim" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
