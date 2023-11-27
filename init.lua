a325ffca93d4a5b137fce544f53c34b0f7f174a4
aik2 <aik2mlj@gmail.com>
2023-11-27 23:22:00 +0400

    use ruff & remove codespell

--- init.lua
+++ init.lua
@@ -42,8 +42,8 @@ return {
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         "lua_ls",
-        "ruff_lsp",
-        "codespell",
+        -- "ruff_lsp",
+        -- "codespell",
       },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
